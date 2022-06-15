49e827c86e277463cb0e080dc61bca584b7c5f23
Tran Ng <tranntn269@gmail.com>
2022-06-15 23:10:08 +0700

    upload code

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {Course, sortCoursesBySeqNo} from '../model/course';
+import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
+import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import { CoursesService } from '../services/courses.service';
+
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  beginnerCourses$: Observable<Course[]>;
+
+  advancedCourses$: Observable<Course[]>;
+
+
+  constructor(
+    private coursesService: CoursesService, 
+    ) {
+
+  }
+
+  ngOnInit() {
+
+   const courses$ = this.coursesService.loadAllCourses().pipe(
+     map(courses => courses.sort(sortCoursesBySeqNo))
+   );
+
+   courses$.subscribe(val => console.log(val))
+
+   this.beginnerCourses$ = courses$.pipe(
+     map(
+       courses => courses.filter(course => course.category == 'BEGINNER')
+     )
+   )
+
+   this.advancedCourses$ = courses$.pipe(map(
+     course => course.filter(course => course.category == 'ADVANCED')
+   ))
+  }
+
+}
+
+
+
+
